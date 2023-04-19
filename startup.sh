cd84558259c803a848a329790f94ee0e408e59a9
arbetamedsarthak <121006829+arbetamedsarthak@users.noreply.github.com>
2023-04-19 11:33:32 +0530

    Update Docker Configuration (#5)
    * updated configuration for minio
    
    * add startup.sh bash script file
    
    * add startup.sh bash script file
    
    * updated startup.sh & dev.yaml
    
    * updated minio container
    
    * update minio url
    
    * update restart policy of db containers
    
    * cleanup docker configuration
    
    * removed private keys

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "Start the content mod docker containers"
+
+cd ~/linked-trust-content-mod
+git pull
+docker-compose --env-file=.env.development -f dev.yaml pull
+docker-compose --env-file=.env.development -f dev.yaml up -d
