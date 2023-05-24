380d38c6c5ae298fdc814766f17e13b267bb1095
tra371 <thuraaung.variable@gmail.com>
2023-05-24 09:32:17 +0630

    make assigning multiple workers at once to a job possible

--- launcher/client/src/components/JobsTable.tsx
+++ launcher/client/src/components/JobsTable.tsx
@@ -18,6 +18,9 @@ import {
   Box,
   Typography,
   TextField,
+  Stack,
+  Chip,
+  InputAdornment,
 } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
@@ -47,8 +50,8 @@ interface JobTableProps {
   onBackButtonClick: () => void;
 }
 
-interface Workers {
-  workerAddress: string;
+interface ChipData {
+  label: string;
 }
 
 export const JobTable = ({
@@ -65,6 +68,8 @@ export const JobTable = ({
   const [workerAddress, setWorkerAddress] = useState('');
   const [workerLoading, setwWorkerLoading] = useState(false);
   const swaggerUrl = `${process.env.REACT_APP_JOB_LAUNCHER_SERVER_URL}/api-docs`;
+  const [chips, setChips] = useState<ChipData[]>([]);
+  const [addressUpdated, setAddressUpdated] = useState(false);
 
   useEffect(() => {
     const fetchJobs = async () => {
@@ -80,9 +85,11 @@ export const JobTable = ({
     fetchJobs();
   }, [activeGroupId]);
 
-  if (isLoading) {
-    return <CircularProgress />;
-  }
+  useEffect(() => {
+    if (!addressUpdated) {
+      setAddressUpdated(true);
+    }
+  }, [chips]);
 
   const handleGenerateApiKey = () => {
     // Call API to generate API key
@@ -102,21 +109,21 @@ export const JobTable = ({
     setInfoDialogOpen(false);
   };
 
-  const handleOpenAddWorkerPopup = () => setOpenAddWorkerPopup(true);
+  const handleOpenAddWorkerPopup = () => {
+    setChips([]);
+    setOpenAddWorkerPopup(true);
+  };
   const handleCloseAddWorkerPopup = () => setOpenAddWorkerPopup(false);
 
   const handleAddWorker = async () => {
     setwWorkerLoading(true);
     try {
-      const isValidAddress = await validateAddress(workerAddress);
-      if (!isValidAddress) {
-        alert('Please enter a valid Ethereum address');
-        return;
-      }
+      const workerAddresses: string[] = [];
+      chips.forEach((chip) => workerAddresses.push(chip.label));
       const response = await axiosInstance.post(
         `/api/groups/${activeGroupId}/add-workers/`,
         {
-          addresses: [workerAddress],
+          addresses: workerAddresses,
         }
       );
 
@@ -134,6 +141,7 @@ export const JobTable = ({
       setWorkerAddress('');
     }
   };
+
   const validateAddress = (address: string) => {
     // Check if the address is a valid Ethereum address using a regular expression
     const regex = /^0x[a-fA-F0-9]{40}$/;
@@ -153,145 +161,214 @@ export const JobTable = ({
     window.open(swaggerUrl, '_blank');
   };
 
+  const handleAdd = () => {
+    const isValidAddress = validateAddress(workerAddress);
+    if (!isValidAddress) {
+      alert('Please enter a valid Ethereum address');
+      return;
+    }
+    if (!chips.find((chip) => chip.label === workerAddress) && workerAddress) {
+      setChips([...chips, { label: workerAddress }]);
+      setWorkerAddress('');
+    } else {
+      alert('Address is already added.');
+    }
+  };
+
+  const handleEnterKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== 'Enter') return;
+    event.preventDefault();
+    handleAdd();
+  };
+
+  const handleAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWorkerAddress(event.target.value);
+    event.preventDefault();
+  };
+
+  const handleChipDelete = (currentChip: string) => {
+    setChips(chips.filter((item) => item.label !== currentChip));
+    console.log(currentChip);
+  };
+
   return (
-    <div className={classes.container}>
-      <Box>
-        <Button sx={{ marginBottom: '10px' }} onClick={onBackButtonClick}>
-          <ArrowBackIcon />
-          <Typography sx={{ marginLeft: '10px' }}>Back to dashboard</Typography>
-        </Button>
-      </Box>
-      <TableContainer component={Paper} className={classes.table}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell>Escrow Address</TableCell>
-              <TableCell>Fund Amount</TableCell>
-              <TableCell>Reviewers Required</TableCell>
-              <TableCell>Reviewers Completed</TableCell>
-              <TableCell>Created AT</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {jobs.map((job) => (
-              <TableRow key={job.id}>
-                <TableCell>{job.title}</TableCell>
-                <TableCell>{job.description}</TableCell>
-                <TableCell>{job.escrowAddress}</TableCell>
-                <TableCell>{job.fundAmount}</TableCell>
-                <TableCell>{job.reviewersRequired}</TableCell>
-                <TableCell>{job.reviewCount}</TableCell>
-                <TableCell>{job.createdAt}</TableCell>
-                <TableCell>{job.status}</TableCell>
-                <TableCell>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    size="small"
-                    onClick={() =>
-                      console.log(`Action button clicked for ${job.title}`)
-                    }
-                  >
-                    View Report
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleGenerateApiKey}
-        sx={{ marginRight: '10px' }}
-      >
-        Generate API Key
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleCreateJob}
-        sx={{ marginRight: '10px' }}
-      >
-        Create Job
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleOpenAddWorkerPopup}
-      >
-        Add Worker
-      </Button>
-      <Dialog open={dialogOpen} onClose={handleCloseDialog}>
-        <DialogTitle>{'API Key'}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>{`Your API Key is: ${apiKey}. Please copy and save it for future use.`}</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary" autoFocus>
-            OK
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Dialog open={infoDialogOpen} onClose={handleCloseInfoDialog}>
-        <DialogTitle>{'API Docs'}</DialogTitle>
-        <DialogContent>
-          <p>
-            To create a new job, please execute the endpoint from the swagger
-            with the required details:
-          </p>
-          <ul style={{ listStyleType: 'decimal' }}>
-            <li>Create New Group.</li>
-            <li>Generate New API Key.</li>
-            <li>Open Swagger({swaggerUrl})</li>
-            <li>Execute the Create Job Endpoint to start creating jobs</li>
-          </ul>
-        </DialogContent>
-        <DialogActions>
+    <>
+      {isLoading && <CircularProgress />}
+      {!isLoading && (
+        <div className={classes.container}>
+          <Box>
+            <Button sx={{ marginBottom: '10px' }} onClick={onBackButtonClick}>
+              <ArrowBackIcon />
+              <Typography sx={{ marginLeft: '10px' }}>
+                Back to dashboard
+              </Typography>
+            </Button>
+          </Box>
+          <TableContainer component={Paper} className={classes.table}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Title</TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell>Escrow Address</TableCell>
+                  <TableCell>Fund Amount</TableCell>
+                  <TableCell>Reviewers Required</TableCell>
+                  <TableCell>Reviewers Completed</TableCell>
+                  <TableCell>Created AT</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {jobs.map((job) => (
+                  <TableRow key={job.id}>
+                    <TableCell>{job.title}</TableCell>
+                    <TableCell>{job.description}</TableCell>
+                    <TableCell>{job.escrowAddress}</TableCell>
+                    <TableCell>{job.fundAmount}</TableCell>
+                    <TableCell>{job.reviewersRequired}</TableCell>
+                    <TableCell>{job.reviewCount}</TableCell>
+                    <TableCell>{job.createdAt}</TableCell>
+                    <TableCell>{job.status}</TableCell>
+                    <TableCell>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                        onClick={() =>
+                          console.log(`Action button clicked for ${job.title}`)
+                        }
+                      >
+                        View Report
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
           <Button
             variant="contained"
-            onClick={handleOpenSwagger}
             color="primary"
-            autoFocus
+            onClick={handleGenerateApiKey}
+            sx={{ marginRight: '10px' }}
           >
-            Open Swagger
+            Generate API Key
           </Button>
           <Button
             variant="contained"
-            onClick={handleCloseInfoDialog}
             color="primary"
-            autoFocus
+            onClick={handleCreateJob}
+            sx={{ marginRight: '10px' }}
           >
-            Close
+            Create Job
           </Button>
-        </DialogActions>
-      </Dialog>
-      <Dialog open={openAddWorkerPopup} onClose={handleCloseAddWorkerPopup}>
-        <DialogTitle>Add Workers</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="address"
-            label="Address"
-            type="text"
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleOpenAddWorkerPopup}
+          >
+            Add Worker
+          </Button>
+          <Dialog open={dialogOpen} onClose={handleCloseDialog}>
+            <DialogTitle>{'API Key'}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>{`Your API Key is: ${apiKey}. Please copy and save it for future use.`}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseDialog} color="primary" autoFocus>
+                OK
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Dialog open={infoDialogOpen} onClose={handleCloseInfoDialog}>
+            <DialogTitle>{'API Docs'}</DialogTitle>
+            <DialogContent>
+              <p>
+                To create a new job, please execute the endpoint from the
+                swagger with the required details:
+              </p>
+              <ul style={{ listStyleType: 'decimal' }}>
+                <li>Create New Group.</li>
+                <li>Generate New API Key.</li>
+                <li>Open Swagger({swaggerUrl})</li>
+                <li>Execute the Create Job Endpoint to start creating jobs</li>
+              </ul>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                variant="contained"
+                onClick={handleOpenSwagger}
+                color="primary"
+                autoFocus
+              >
+                Open Swagger
+              </Button>
+              <Button
+                variant="contained"
+                onClick={handleCloseInfoDialog}
+                color="primary"
+                autoFocus
+              >
+                Close
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Dialog
+            open={openAddWorkerPopup}
+            onClose={handleCloseAddWorkerPopup}
             fullWidth
-            value={workerAddress}
-            onChange={(event) => setWorkerAddress(event.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseAddWorkerPopup}>Cancel</Button>
-          <Button onClick={handleAddWorker} color="primary">
-            {workerLoading ? <CircularProgress size={24} /> : 'Add'}
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+            maxWidth="sm"
+          >
+            <DialogTitle>Add Workers</DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="address"
+                label="Address"
+                type="text"
+                fullWidth
+                value={workerAddress}
+                onKeyDown={handleEnterKeyDown}
+                onChange={handleAddressChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Button onClick={handleAdd}>Add</Button>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Stack
+                spacing={1}
+                useFlexGap
+                direction="row"
+                flexWrap="wrap"
+                sx={{ mt: 2 }}
+              >
+                {addressUpdated
+                  ? chips.map((chip) => {
+                      return (
+                        <Chip
+                          sx={{ fontSize: 16 }}
+                          label={chip.label}
+                          onDelete={() => handleChipDelete(chip.label)}
+                        />
+                      );
+                    })
+                  : ''}
+              </Stack>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseAddWorkerPopup}>Cancel</Button>
+              <Button onClick={handleAddWorker} color="primary">
+                {workerLoading ? <CircularProgress size={24} /> : 'Submit'}
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      )}
+    </>
   );
 };
